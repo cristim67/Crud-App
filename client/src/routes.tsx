407f8e67e05b12117e0eeec2c194bba9c810217d
cristim67 <miloiuc4@gmail.com>
2023-12-31 11:44:04 +0200

    Add professor logic

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,6 +1,7 @@
 import { HomeIcon } from "@heroicons/react/24/solid";
 import { Students } from "./pages/dashboard/students.tsx";
 import { Subjects } from "./pages/dashboard/subjects.tsx";
+import { Professors } from "./pages/dashboard/professors.tsx";
 
 const icon = {
   className: "w-5 h-5 text-inherit",
@@ -26,6 +27,17 @@ export const routes = [
         name: "Subjects",
         path: "/subjects",
         element: <Subjects />,
+      },
+    ],
+  },
+  {
+    layout: "dashboard",
+    pages: [
+      {
+        icon: <HomeIcon {...icon} />,
+        name: "Professors",
+        path: "/professors",
+        element: <Professors />,
       },
     ],
   },
